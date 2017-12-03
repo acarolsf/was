f5497e453a74402b64c10d0fd0a0e3a8aa0b48a4
Ana Carolina <cscarol20@gmail.com>
2017-12-03 16:17:28 -0200

    Update bd WAS.sql

--- database/bd WAS.sql
+++ database/bd WAS.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE db_pweb;
-USE db_pweb;
+CREATE DATABASE db_was;
+USE db_was;
 
-CREATE TABLE db_pweb.tb_corretora(
+CREATE TABLE tb_corretora(
     id_corretora int not null auto_increment,
     nome varchar(200),
     cidade varchar(200),
@@ -11,16 +11,14 @@ CREATE TABLE db_pweb.tb_corretora(
     primary key(id_corretora));
 
 
-CREATE TABLE db_pweb.tb_usuario(
+CREATE TABLE tb_usuario(
 	id int not null auto_increment,
     login VARCHAR(50),
     senha VARCHAR(50),
     email VARCHAR(50),
     primary key(id));
     
-INSERT INTO db_pweb.tb_usuario (login, senha, email) VALUES ('admin', '1234', 'cscarol20@gmail.com');
-INSERT INTO db_pweb.tb_corretora (nome, cidade,telefone,email, cnpj) VALUES ('Corretora ImoveiSerido', 'Jardim do Serido', '49215678', 'jardimoveis@corretora.com',
+INSERT INTO tb_usuario (login, senha, email) VALUES ('admin', '1234', 'cscarol20@gmail.com');
+INSERT INTO tb_corretora (nome, cidade,telefone,email, cnpj) VALUES ('Corretora ImoveiSerido', 'Jardim do Serido', '49215678', 'jardimoveis@corretora.com',
  '321456766');
 
-select * from db_pweb.tb_corretora;
-select * from db_pweb.tb_usuario;
